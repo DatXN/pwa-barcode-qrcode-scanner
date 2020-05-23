ae564a6d98df06b0f29ab5f44ce8b58e21ff21bd
DatXN <datnguyen231@gmail.com>
2020-05-23 16:18:12 +0700

    update barcode reader detector

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -33,6 +33,10 @@ const Video = (props) => {
           },
           numOfWorkers: 4,
           locate: true,
+          locator: {
+            patchSize: "medium",
+            halfSample: true,
+          },
           decoder: {
             readers: [
               "ean_reader",
@@ -47,7 +51,6 @@ const Video = (props) => {
               showPattern: true,
             },
           },
-          debug: true,
         },
         (err) => {
           if (err) {
@@ -58,6 +61,49 @@ const Video = (props) => {
         }
       );
       Quagga.onDetected(onDetected);
+      Quagga.onProcessed(function (result) {
+        var drawingCtx = Quagga.canvas.ctx.overlay,
+          drawingCanvas = Quagga.canvas.dom.overlay;
+
+        if (result) {
+          if (result.boxes) {
+            console.log(drawingCtx);
+
+            drawingCtx.clearRect(
+              0,
+              0,
+              parseInt(drawingCanvas.getAttribute("width")),
+              parseInt(drawingCanvas.getAttribute("height"))
+            );
+            result.boxes
+              .filter(function (box) {
+                return box !== result.box;
+              })
+              .forEach(function (box) {
+                Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+                  color: "green",
+                  lineWidth: 2,
+                });
+              });
+          }
+
+          if (result.box) {
+            Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+              color: "#00F",
+              lineWidth: 2,
+            });
+          }
+
+          if (result.codeResult && result.codeResult.code) {
+            Quagga.ImageDebug.drawPath(
+              result.line,
+              { x: "x", y: "y" },
+              drawingCtx,
+              { color: "red", lineWidth: 3 }
+            );
+          }
+        }
+      });
     }
   }, []);
 
